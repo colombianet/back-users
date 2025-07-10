9530e3be14644cf2b1f926b794abc47b23eae445
Oscar Perez <ojperez@bancolombia.com.co>
2025-07-10 16:17:47 -0500

    Actualizacion en el query para q solo devuelva agentes

--- src/main/java/com/inmohouse/backend/backend/repositories/PropiedadRepository.java
+++ src/main/java/com/inmohouse/backend/backend/repositories/PropiedadRepository.java
@@ -14,7 +14,14 @@ public interface PropiedadRepository extends CrudRepository<Propiedad, Long> {
     @Query("SELECT p.tipo, COUNT(p) FROM Propiedad p GROUP BY p.tipo")
     List<Object[]> contarPorTipo();
 
-    @Query("SELECT p.agente.nombre, COUNT(p) FROM Propiedad p GROUP BY p.agente.nombre")
+    // No devuelve usuarios con otros roles q no sean agente aunque tengan propiedades asignadas
+    @Query("""
+        SELECT p.agente.nombre, COUNT(p)
+        FROM Propiedad p
+        JOIN p.agente.roles r
+        WHERE r.nombre = 'ROLE_AGENTE'
+        GROUP BY p.agente.nombre
+    """)
     List<Object[]> contarPorAgente();
 
     @Query("""
