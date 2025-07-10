98c0f9e508f1a7d01445133d133b89b0ed774957
Oscar Perez <ojperez@bancolombia.com.co>
2025-07-10 09:29:01 -0500

    Redeploy por cambio en URL front

--- src/main/java/com/inmohouse/backend/backend/security/SecurityConfig.java
+++ src/main/java/com/inmohouse/backend/backend/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
             "http://localhost:4200",
-            "https://front-users-inmohouse.vercel.app"
+            "https://front-users-inmohouse-jkni.vercel.app/"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
